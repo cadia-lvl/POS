fe17c6e337ddff7b888367c324c603439ad84091
Haukur Pall Jonsson <haukurpalljonsson@gmail.com>
2020-08-06 11:42:54 +0000

    Removing tag examples to simplify accuracy calculations.

--- pos/evaluate.py
+++ pos/evaluate.py
@@ -1,5 +1,4 @@
 """A collection of types and function used to evaluate the performance of a tagger."""
-from collections import Counter
 from typing import Tuple, List, Dict, Set
 import logging
 from pathlib import Path
@@ -8,105 +7,6 @@ import pickle
 from . import data
 
 log = logging.getLogger()
-
-Example = Tuple[str, str, str, int]
-
-
-class TagExamples:
-    """A class to represent tags and their occurrances."""
-
-    def __init__(self, tag: str):
-        """Initialize the object."""
-        self.tag = tag
-        self.examples: List[Tuple[str, str, int]] = []
-
-    def add_example(self, token, predicted, line_id) -> None:
-        """Add an example of tag usage. A usage is when it is the correct tag."""
-        self.examples.append((token, predicted, line_id))
-
-    def __len__(self) -> int:
-        """Return the number of examples of this tag."""
-        return len(self.examples)
-
-    def incorrect_count(self) -> int:
-        """Return the number of occurance a tag is incorrectly predicted."""
-        count = 0
-        for _, p, _ in self.examples:
-            if self.tag != p:
-                count += 1
-        return count
-
-    def errors(self) -> Counter:
-        """Return a Counter for the types of errors for a given tag."""
-        errors: Counter = Counter()
-        for _, p, _ in self.examples:
-            if self.tag != p:
-                errors.update([f"{p} -> {self.tag}"])
-        return errors
-
-
-def flatten_data(in_data: data.PredictedDataset) -> List[Example]:
-    """Flatten the predictions to a sequence of tokens along with their tags."""
-    return [
-        (tok, gold, pred, sent_idx)
-        for tok, gold, pred, sent_idx, _ in in_data.as_sequence()
-    ]
-
-
-def analyse_examples(flat_data: List[Example]) -> Dict[str, TagExamples]:
-    """Run TagExamples on each token and tag."""
-    examples: Dict[str, TagExamples] = {}
-    for tok, gold, pred, line_id in flat_data:
-        try:
-            examples[gold].add_example(tok, pred, line_id)
-        except KeyError:
-            examples[gold] = TagExamples(gold)
-            examples[gold].add_example(tok, pred, line_id)
-    return examples
-
-
-def calculate_accuracy(examples: Dict[str, TagExamples]) -> Tuple[float, int]:
-    """Calculate the total and incorrect count of the TagExamples."""
-    total = 0
-    incorrect = 0
-    for gold, tag_example in examples.items():
-        total += len(tag_example)
-        incorrect += tag_example.incorrect_count()
-    if total == 0:
-        return (0.0, 0)
-    return ((total - incorrect) / total, total)
-
-
-def all_errors(examples: Dict[str, TagExamples]) -> Counter:
-    """Return a Counter of all the errors."""
-    error_counter: Counter = Counter()
-    for gold, tag_example in examples.items():
-        error_counter.update(tag_example.errors())
-    return error_counter
-
-
-def get_vocab(examples: Dict[str, TagExamples]) -> Set[str]:
-    """Return the vocabulary of the TagExamples."""
-    vocab: Set[str] = set()
-    for tag_example in examples.values():
-        vocab.update(set(example[0] for example in tag_example.examples))
-    return vocab
-
-
-def filter_examples(
-    examples: Dict[str, TagExamples], vocab: Set[str]
-) -> Dict[str, TagExamples]:
-    """Filter the TagExamples based on a vocabulary."""
-    filtered: Dict[str, TagExamples] = {}
-    for tag, tag_example in examples.items():
-        for example in tag_example.examples:
-            if example[0] in vocab:
-                try:
-                    filtered[tag].add_example(*example)
-                except KeyError:
-                    filtered[tag] = TagExamples(tag)
-                    filtered[tag].add_example(*example)
-    return filtered
 
 
 class Experiment:
@@ -119,8 +19,9 @@ class Experiment:
         dicts: Dict[str, data.VocabMap],
     ):
         """Initialize an experiment given the predictions and vocabulary."""
-        self.examples = analyse_examples(flatten_data(predictions))
-        self.test_vocab = get_vocab(self.examples)
+        self.predictions = predictions
+        # Get the tokens and retrive the vocab.
+        self.test_vocab = data.Vocab.from_symbols(self.predictions.unpack()[0])
         self.known_vocab = train_vocab.intersection(self.test_vocab)
         self.dicts = dicts
 
@@ -137,8 +38,8 @@ class Experiment:
             )
         else:
             wemb_vocab = set()
-        self.unknown_vocab = self.test_vocab.difference(self.known_vocab)
         # fmt: off
+        self.unknown_vocab = self.test_vocab.difference(self.known_vocab)  # pylint: disable=no-member
         self.known_wemb_vocab = self.known_vocab.intersection(wemb_vocab).difference(morphlex_vocab)
         self.known_wemb_morphlex_vocab = self.known_vocab.intersection(wemb_vocab).intersection(morphlex_vocab)
         self.known_morphlex_vocab = self.known_vocab.intersection(morphlex_vocab).difference(wemb_vocab)
@@ -166,9 +67,41 @@ class Experiment:
     def accuracy(self, vocab: Set[str] = None) -> Tuple[float, int]:
         """Calculate the accuracy given a vocabulary to filter on. If nothing is provided, we do not filter."""
         if vocab is None:
-            return calculate_accuracy(self.examples)
-        else:
-            return calculate_accuracy(filter_examples(self.examples, vocab=vocab))
+            total = sum(
+                (
+                    1
+                    for tokens, gold_tags, predicted_tags in self.predictions
+                    for _, gold, predicted in zip(tokens, gold_tags, predicted_tags)
+                )
+            )
+            correct = sum(
+                (
+                    predicted == gold
+                    for tokens, gold_tags, predicted_tags in self.predictions
+                    for _, gold, predicted in zip(tokens, gold_tags, predicted_tags)
+                )
+            )
+            return (correct / total, total)
+        # We need to filter based on the given vocabulary
+        total = sum(
+            (
+                1
+                for tokens, gold_tags, predicted_tags in self.predictions
+                for token, gold, predicted in zip(tokens, gold_tags, predicted_tags)
+                if token in vocab
+            )
+        )
+        if total == 0:
+            return (0.0, 0)
+        correct = sum(
+            (
+                predicted == gold
+                for tokens, gold_tags, predicted_tags in self.predictions
+                for token, gold, predicted in zip(tokens, gold_tags, predicted_tags)
+                if token in vocab
+            )
+        )
+        return (correct / total, total)
 
     def all_accuracy(self):
         """Return all accuracies."""
