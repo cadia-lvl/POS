eb70e2c2ac437b5b38b3b2a204d7d01456dd4876
Haukur Pall Jonsson <haukurpalljonsson@gmail.com>
2020-09-04 11:41:39 +0000

    Updating deps

--- bin/full_model.sh
+++ bin/full_model.sh
@@ -14,25 +14,24 @@ if ((FIRST_STEP <= 1 && LAST_STEP >= 1)); then
     --output="$out_folder/slurm-%j.out" \
     --gres=gpu \
     --mem=30G \
-    --wrap="./main.py \
+    --wrap="pos \
     train-and-tag \
     $DATA_DIR/otb/01TM.plain \
     $DATA_DIR/otb/01PM.plain \
-    $DATA_DIR/mim/01TM.plain \
-    $DATA_DIR/mim/01PM.plain \
+    $DATA_DIR/mim/10TM.plain \
+    $DATA_DIR/mim/10PM.plain \
     $out_folder \
-    --epochs 25 \
+    --pretrained_word_embeddings_file data/extra/igc2018.vec \
+    --morphlex_embeddings_file data/extra/dmii.vectors \
+    --morphlex_freeze \
+    --known_chars_file data/extra/characters_training.txt \
+    --main_lstm_layers 2 \
+    --label_smoothing 0.1 \
+    --epochs 40 \
     --batch_size 16 \
     --save_vocab \
     --save_model \
     --gpu \
     --optimizer sgd \
-    --pretrained_word_embeddings_file data/extra/igc2018.vec \
-    --morphlex_embeddings_file data/extra/dmii.vectors \
-    --morphlex_freeze \
-    --final_dim 64 \
-    --main_lstm_layers 2 \
-    --label_smoothing 0.1 \
-    --word_embedding_lr 0.0 \
     --learning_rate 0.2"
 fi
