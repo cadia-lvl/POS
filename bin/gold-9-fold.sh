46393fe95ec8e8202468e83304d3c4e30f5ed333
Haukur Pall Jonsson <haukurpalljonsson@gmail.com>
2020-09-12 17:17:36 +0000

    Training parameters for cross fold

--- bin/gold-9-fold.sh
+++ bin/gold-9-fold.sh
@@ -21,12 +21,11 @@ if ((FIRST_STEP <= 1 && LAST_STEP >= 1)); then
     for fold in $FOLDS; do
         out_folder=./out/"$NAME"/$fold
         mkdir -p "$out_folder"
-        extra_params="$*"
         sbatch \
         --output="$out_folder/slurm-%j.out" \
         --gres=gpu \
         --mem=10G \
-        --wrap="./main.py \
+        --wrap="pos \
         train-and-tag \
         $DATA_DIR/${fold}TM.plain \
         $DATA_DIR/${fold}PM.plain \
@@ -35,14 +34,14 @@ if ((FIRST_STEP <= 1 && LAST_STEP >= 1)); then
         --morphlex_freeze \
         --pretrained_word_embeddings_file data/extra/igc2018.vec_filtered \
         --known_chars_file data/extra/characters_training.txt \
-        --final_dim 32 \
-        --learning_rate 0.2 \
-        --optimizer sgd \
-        --epochs 50 \
+        --main_lstm_layers 2 \
+        --label_smoothing 0.1 \
+        --epochs 40 \
         --batch_size 16 \
         --save_vocab \
         --save_model \
         --gpu \
-        $extra_params"
+        --optimizer sgd \
+        --learning_rate 0.2"
     done
 fi
--- bin/ifd+gold-9-fold.sh
+++ bin/ifd+gold-9-fold.sh
@@ -23,13 +23,18 @@ if ((FIRST_STEP <= 1 && LAST_STEP >= 1)); then
         $DATA_DIR/mim/${fold}TM.plain \
         $DATA_DIR/mim/${fold}PM.plain \
         $out_folder \
-        --epochs 20 \
+        --morphlex_embeddings_file data/extra/dmii.vectors_filtered \
+        --morphlex_freeze \
+        --pretrained_word_embeddings_file data/extra/igc2018.vec_filtered \
+        --known_chars_file data/extra/characters_training.txt \
+        --main_lstm_layers 2 \
+        --label_smoothing 0.1 \
+        --epochs 40 \
         --batch_size 16 \
         --save_vocab \
         --save_model \
         --gpu \
         --optimizer sgd \
-        --learning_rate 0.2 \
-        --final_dim 32"
+        --learning_rate 0.2"
     done
 fi
