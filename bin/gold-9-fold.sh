3d5e3f90194b1519508838d293825a7ea7c71543
Haukur <haukurpalljonsson@gmail.com>
2020-12-07 10:25:57 +0000

    Accepting model as input

--- bin/gold-9-fold.sh
+++ bin/gold-9-fold.sh
@@ -2,9 +2,10 @@
 FOLDS="01 02 03 04 05 06 07 08 09"
 DATA_DIR=./data/raw/mim
 
-NAME="$1"
+MODEL=$1
+NAME="$2"
 # Move the arguments forward
-shift
+shift; shift
 
 # --begin=now+8hour \
 for fold in $FOLDS
@@ -12,5 +13,5 @@ do
     TRAIN=$DATA_DIR/${fold}TM.plain
     TEST=$DATA_DIR/${fold}PM.plain
     OUT_DIR=./out/"$NAME"/$fold
-    ./bin/wrap_sbatch.sh $OUT_DIR $TRAIN $TEST $*
+    ./bin/wrap_sbatch.sh $MODEL $OUT_DIR $TRAIN $TEST $*
 done
