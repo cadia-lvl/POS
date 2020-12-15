3a560448c96d9f9b696155229ca28515ded78bad
Haukur <haukurpalljonsson@gmail.com>
2020-12-15 14:56:57 +0000

    Residual connections now correct

--- bin/roberta_tagger.sh
+++ bin/roberta_tagger.sh
@@ -34,7 +34,6 @@ train-and-tag \
 --bert_encoder roberta \
 --bert_encoder_dim 768 \
 --bert_encoder_length 514 \
---bert_to_bilstm \
 --main_lstm_dim 384 \
 --main_lstm_layers 1 \
 --label_smoothing 0.1 \
