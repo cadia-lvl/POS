30c11ade4cb9ddb4f4e3c62fb04f608cf9ce3ffb
Haukur <haukurpalljonsson@gmail.com>
2020-12-21 15:50:47 +0000

    lemmatizer-char+bert

--- bin/char_bert_lemmatizer.sh
+++ bin/char_bert_lemmatizer.sh
@@ -30,7 +30,7 @@ train-and-tag \
 --char_lstm_layers 1 \
 --char_emb_dim 64 \
 --main_lstm_layers 1 \
---main_lstm_dim 512 \
+--main_lstm_dim 256 \
 --label_smoothing 0.1 \
 --epochs 10 \
 --batch_size 8 \
