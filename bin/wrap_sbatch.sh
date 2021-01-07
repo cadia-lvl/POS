d4f479a9675e34daf0266de5a504385d3467e485
Haukur <haukurpalljonsson@gmail.com>
2021-01-07 13:36:22 +0000

    Roberta-all experiment

--- bin/wrap_sbatch.sh
+++ bin/wrap_sbatch.sh
@@ -3,12 +3,11 @@ MODEL=$1
 OUT_DIR=$2
 TRAIN=$3
 TEST=$4
-
 mkdir -p $OUT_DIR
-# Pop the model.sh
-shift
+# Pop the model.sh, out_dir, train and test
+shift; shift; shift; shift
 sbatch \
 --output="$OUT_DIR/slurm-%j.out" \
 --gres=gpu \
 --mem=10G \
---wrap="$MODEL $* --gpu"
+--wrap="$MODEL $TRAIN $TEST $OUT_DIR $* --gpu"
