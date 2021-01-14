a8f23d284cd6968484c4058e2b305efd3e1bfb76
Haukur <haukurpalljonsson@gmail.com>
2021-01-14 17:00:32 +0000

    IFD 9 run config

--- bin/ifd-9-fold.sh
+++ bin/ifd-9-fold.sh
@@ -1,38 +1,17 @@
 #!/bin/bash
 FOLDS="01 02 03 04 05 06 07 08 09"
-DATA_DIR=./data/raw
+DATA_DIR=./data/raw/otb
 
-NAME="$1"
+MODEL=$1
+NAME="$2"
 # Move the arguments forward
-shift
+shift; shift
 
-FIRST_STEP=1
-LAST_STEP=1
-if ((FIRST_STEP <= 1 && LAST_STEP >= 1)); then
-    for fold in $FOLDS; do
-        out_folder=./out/"$NAME"/$fold
-        mkdir -p "$out_folder"
-        sbatch \
-        --output="$out_folder/slurm-%j.out" \
-        --gres=gpu \
-        --mem=10G \
-        --wrap="pos \
-        train-and-tag \
-        $DATA_DIR/otb/${fold}TM.plain \
-        $DATA_DIR/otb/${fold}PM.plain \
-        $out_folder \
-        --morphlex_embeddings_file data/extra/dmii.vectors_filtered \
-        --morphlex_freeze \
-        --pretrained_word_embeddings_file data/extra/igc2018.vec_filtered \
-        --known_chars_file data/extra/characters_training.txt \
-        --main_lstm_layers 2 \
-        --label_smoothing 0.1 \
-        --epochs 40 \
-        --batch_size 16 \
-        --save_vocab \
-        --save_model \
-        --gpu \
-        --optimizer sgd \
-        --learning_rate 0.2"
-    done
-fi
+# --begin=now+8hour \
+for fold in $FOLDS
+do
+    TRAIN=$DATA_DIR/${fold}TM.plain
+    TEST=$DATA_DIR/${fold}PM.plain
+    OUT_DIR=./out/"$NAME"/$fold
+    ./bin/wrap_sbatch.sh $MODEL $OUT_DIR $TRAIN $TEST $*
+done
