46b4e6193b1f470e4ed61dbf0a8d24243d8ecba7
Haukur <haukurpalljonsson@gmail.com>
2021-01-15 15:58:57 +0000

    MIM_OTB

--- bin/mim_otb-10-fold.sh
+++ bin/mim_otb-10-fold.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
 FOLDS="01 02 03 04 05 06 07 08 09 10"
-DATA_DIR=./data/raw
+DATA_DIR=./data/raw/mim_otb
 
-NAME="$1"
+MODEL=$1
+NAME="$2"
 # Move the arguments forward
-shift
+shift; shift
 
-FIRST_STEP=1
-LAST_STEP=1
-if ((FIRST_STEP <= 1 && LAST_STEP >= 1)); then
-    for fold in $FOLDS; do
-        out_folder=./out/"$NAME"/$fold
-        mkdir -p "$out_folder"
-        sbatch \
-        --output="$out_folder/slurm-%j.out" \
-        --gres=gpu \
-        --mem=10G \
-        --wrap="pos \
-        train-and-tag \
-        $DATA_DIR/mim_otb/${fold}TM.plain \
-        $DATA_DIR/mim_otb/${fold}PM.plain \
-        $out_folder \
-        --bert_encoder electra_model \
-        --main_lstm_layers 0 \
-        --final_layer none \
-        --label_smoothing 0.1 \
-        --epochs 25 \
-        --batch_size 16 \
-        --save_vocab \
-        --save_model \
-        --gpu \
-        --optimizer adam \
-        --learning_rate 5e-5"
-    done
-fi
+# --begin=now+8hour \
+for fold in $FOLDS
+do
+    TRAIN=$DATA_DIR/${fold}TM.plain
+    TEST=$DATA_DIR/${fold}PM.plain
+    OUT_DIR=./out/"$NAME"/$fold
+    ./bin/wrap_sbatch.sh $MODEL $OUT_DIR $TRAIN $TEST $*
+done
