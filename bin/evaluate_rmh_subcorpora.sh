c92c3f01751c4cffe4e4d7bf9c66bc8a90a945d6
Haukur <haukurpalljonsson@gmail.com>
2021-01-18 14:11:17 +0000

    RMH subcorpora eval

--- bin/evaluate_rmh_subcorpora.sh
+++ bin/evaluate_rmh_subcorpora.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-MODEL_PATH="out/full-v2"
+MODEL_PATH="out/m4_full"
 OUT_DIR="data/pred"
 for file in data/raw/gull2_/*.plain; do
-    pos tag $MODEL_PATH/tagger.pt $MODEL_PATH/dictionaries.pickle $file $OUT_DIR/"$(basename $file)".pred --contains_tags
+    pos tag $MODEL_PATH/tagger.pt $file $OUT_DIR/"$(basename $file)".pred --contains_tags
 done
