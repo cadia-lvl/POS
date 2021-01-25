f9b6f9ebafb08ba390821a61b78e818890ab32d7
Haukur <haukurpalljonsson@gmail.com>
2021-01-25 12:19:21 +0000

    ABLTagger v1.0 redo

---
+++ bin/abl_tagger_v10.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+OUT_DIR=$1
+TRAIN=$2
+TEST=$3
+
+mkdir -p $OUT_DIR
+shift; shift; shift
+echo $*
+#    --morphlex_embeddings_file data/extra/dmii.vectors_filtered \
+#    --morphlex_freeze \
+#    --pretrained_word_embeddings_file data/extra/igc2018.vec_filtered \
+#    --known_chars_file data/extra/characters_training.txt \
+#    --label_smoothing 0.1 \
+#    --main_lstm_layers 2 \
+#    --word_embedding_dim 128 \
+#    --pretrained_model_folder bull \
+# --lemmatizer \
+# --known_chars_file data/extra/characters_training.txt \
+# --char_lstm_layers 1 \
+# --char_emb_dim 64 \
+# --main_lstm_layers 1 \
+# --main_lstm_dim 128 \
+# --bert_encoder roberta \
+# --bert_encoder_dim 768 \
+pos \
+train-and-tag \
+"$TRAIN" \
+"$TEST" \
+"$OUT_DIR" \
+--tagger \
+--tagger_embedding bilstm \
+--morphlex_embeddings_file data/extra/dmii.vectors_filtered \
+--morphlex_freeze \
+--pretrained_word_embeddings_file data/extra/igc2018.vec_filtered \
+--known_chars_file data/extra/characters_training.txt \
+--char_emb_dim 20 \
+--char_lstm_dim 64 \
+--char_lstm_layers 1 \
+--main_lstm_dim 64 \
+--main_lstm_layers 2 \
+--label_smoothing 0.1 \
+--epochs 40 \
+--batch_size 16 \
+--save_vocab \
+--save_model \
+--optimizer sgd \
+--learning_rate 0.2 \
+$*
