0f89c455b50d0826bd447fba1b5f96887426522f
Haukur <haukurpalljonsson@gmail.com>
2021-01-26 11:35:54 +0000

    No residuals

--- src/pos/cli.py
+++ src/pos/cli.py
@@ -264,6 +264,7 @@ def train_and_tag(**kwargs):
         main_lstm_layers=kwargs["main_lstm_layers"],
         lstm_dropouts=0.0,
         input_dropouts=kwargs["emb_dropouts"],
+        residual=False,
     )
     decoders: Dict[Modules, Decoder] = {}
     if kwargs["tagger"]:
