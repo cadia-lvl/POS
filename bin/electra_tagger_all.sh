b1befa328c6040efb4c9a924ce3ae2ebd3ccab3b
Haukur <haukurpalljonsson@gmail.com>
2021-01-26 15:05:46 +0000

    Electra tagger all larger

--- bin/electra_tagger_all.sh
+++ bin/electra_tagger_all.sh
@@ -39,7 +39,7 @@ train-and-tag \
 --known_chars_file data/extra/characters_training.txt \
 --char_lstm_layers 1 \
 --char_emb_dim 128 \
---main_lstm_dim 128 \
+--main_lstm_dim 256 \
 --main_lstm_layers 1 \
 --label_smoothing 0.1 \
 --epochs 20 \
