6df156ffd865f48a31528f14bed647a539c7c5ff
Haukur <haukurpalljonsson@gmail.com>
2021-01-28 11:27:14 +0000

    RMH eval updated

--- bin/evaluate_rmh_subcorpora.sh
+++ bin/evaluate_rmh_subcorpora.sh
@@ -3,4 +3,5 @@ MODEL_PATH="out/m4_full/10"
 OUT_DIR="data/pred"
 for file in data/raw/gull2_/*.plain; do
     pos tag $MODEL_PATH/tagger.pt $file $OUT_DIR/"$(basename $file)".pred --contains_tags
+    pos evaluate-predictions $OUT_DIR/"$(basename $file)".pred tokens,gold_tags,tags --train_tokens $MODEL_PATH/known_toks.txt
 done
