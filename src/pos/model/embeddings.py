35264fd269e9be79b03c42ce481bd44336f3625a
Haukur <haukurpalljonsson@gmail.com>
2021-04-20 17:05:26 +0000

    Reading max pos from config instead of tokenizer

--- src/pos/model/embeddings.py
+++ src/pos/model/embeddings.py
@@ -151,7 +151,7 @@ class TransformerEmbedding(abltagger.Embedding):
         # ELECTRA property
         self.num_layers = self.config.num_hidden_layers  # type: ignore
         self.hidden_dim = self.config.hidden_size  # type: ignore
-        self.max_length = self.tokenizer.max_len_single_sentence  # type: ignore
+        self.max_length = self.config.max_position_embeddings  # type: ignore
         self.dropout = nn.Dropout(p=dropout)
 
     def preprocess(self, batch: Sequence[core.Sentence]) -> Dict[str, torch.Tensor]:
